370997c962d6fffb9ec0a7a12bb72694d9d590bb
Muia23 <64227473+Muia23@users.noreply.github.com>
2020-05-22 01:25:21 +0300

    add function to adjust quantity and update cart total

--- js/scripts.js
+++ js/scripts.js
@@ -34,3 +34,10 @@ function removeCartItem(event) {
     buttonClicked.parentElement.parentElement.remove()
     updateCartTotal()
 }
+function quantityChanged(event) {
+    var input = event.target
+    if (isNaN(input.value) || input.value <= 0) {
+        input.value = 1
+    }
+    updateCartTotal()
+}
