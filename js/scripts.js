b7a56d7b4247a2d4dd971c89d21fbfebb805be76
Muia23 <64227473+Muia23@users.noreply.github.com>
2020-05-22 01:22:57 +0300

    and an alert function once button is clicked

--- js/scripts.js
+++ js/scripts.js
@@ -21,3 +21,11 @@ function ready() {
     }
   document.getElementsByClassName('btn-purchase')[0].addEventListener('click', purchaseClicked)
 }
+function purchaseClicked() {
+    alert('Thank you for your purchase. Kindly come again')
+    var cartItems = document.getElementsByClassName('cart-items')[0]
+    while (cartItems.hasChildNodes()) {
+        cartItems.removeChild(cartItems.firstChild)
+    }
+    updateCartTotal()
+}
